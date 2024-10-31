d0038b36d2899e964b813ed6a6318bd4eb91fa47
jangh1109 <jang031109@gmail.com>
2024-10-31 12:27:49 +0900

    v2.1 new

---
+++ week10/week10_dataset.py
@@ -0,0 +1,10 @@
+import seaborn as sns
+import pandas as pd
+
+titanic = sns.load_dataset('titanic')
+# print(titanic.describe())
+# print(titanic.head())
+# print(len(titanic))
+# print(titanic.tail(10))
+# print(titanic.info())
+# print(titanic['embarked'], titanic['deck'],titanic['class'])
