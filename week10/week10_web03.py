583b9aa03d6b4afb3fe5aa87111ae0f8783c2e65
jangh1109 <jang031109@gmail.com>
2024-10-31 11:07:00 +0900

    v2.0 new

--- week10/week10_web03.py
+++ week10/week10_web03.py
@@ -1,6 +1,7 @@
 import urllib.request
 import pandas as pd
 from bs4 import BeautifulSoup
+import datetime
 
 shops = []
 
@@ -18,7 +19,7 @@ for i in range(1,51):
         shop_addr = tds[3].string # 주소
         shop_phone = tds[5].string # 전화번호
 
-        shops.append([shop_name]+[shop_addr]+[shop_phone]) # 2d array
+        shops.append([shop_name]+[shop_addr]+[shop_phone]+[datetime.datetime.now().strftime("%Y년 %m월 %d일 %H시 %M분 %S초")]) # 2d array
 
 print(shops)
 hollys_df = pd.DataFrame(shops, columns=('매장이름','주소','전화번호', '일시'))
